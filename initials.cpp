10605e5ae23c7b6172efdcba4226dbe7d0f3255f
Gavin-Mulderig <73673305+Gavin-Mulderig@users.noreply.github.com>
2021-06-03 09:48:27 -0700

    Add files via upload

---
+++ initials.cpp
@@ -0,0 +1,14 @@
+// g++ initaials.cpp -o gjm.o
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<< " GGGG      JJJJ  M     M \n";
+	cout<< "G    G        J  M M M M \n";
+	cout<< "G             J  M  M  M \n";
+	cout<< "G  GGG        J  M     M \n";
+	cout<< "G    G  J    J   M     M \n";
+	cout<< " GGGG     JJJ    M     M \n";
+	
+	return 0;
+}	
